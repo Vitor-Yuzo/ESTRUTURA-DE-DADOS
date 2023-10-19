f1576c07919bf34a865f9427f5ce5dd50e3afed8
Vitor-Yuzo <vitor.yuzo.profissional@gmail.com>
2023-10-19 14:52:28 -0300

    Laboratorio
    Função que retorna quantidade de nos internos em uma arvore

---
+++ Exercicio02_main.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ARVORE.h"
+
+
+int nos_internos(NoArv *pai,int quantidade)
+{
+    *quantidade += 1;
+
+    if(pai->dir != NULL)
+    {
+        nos_internos(pai->dir,quantidade);
+    }
+
+    if(pai->esq != NULL)
+    {
+        nos_internos(pai->esq,quantidade);
+    }
+
+    if(pai->esq == NULL && pai->dir == NULL)
+    {
+        *quantidade -= 1;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+    int i,num;
+    Arv *arvore1 = NULL;
+    int flag;
+    int quantidade;
+
+    arvore1 = CriaArvore(arvore1);
+
+    if(Arvore_Vazia(arvore1))
+    {
+        printf ("\n\nCHECK 1: A ARVORE ESTA VAZIA\n\n");
+    }
+
+    //printf ("Digite um numero para inserir na arvore: ");
+    //scanf  ("%d",&num);
+
+    //insere(arvore1,num);
+    insere(arvore1,50);
+    insere(arvore1,40);
+    insere(arvore1,45);
+    insere(arvore1,30);
+    insere(arvore1,60);
+    insere(arvore1,55);
+    insere(arvore1,70);
+    insere(arvore1,65);
+    insere(arvore1,80);
+
+    if(Arvore_Vazia(arvore1))
+    {
+        printf ("\n\n A ARVORE ESTA VAZIA\n\n");
+    }
+    else
+    {
+        printf ("\n\n -----PRE ORDER------ \n\n");
+        PreOrder(arvore1->raiz);
+
+        printf ("\n\n -----POS ORDER----- \n\n");
+        PosOrder(arvore1->raiz);
+
+        printf ("\n\n -----IN ORDER-----\n\n");
+        InOrder(arvore1->raiz);
+    }
+
+    // BUSCAR
+
+    printf ("\n\n");
+
+    flag = 0;
+    flag = busca(arvore1->raiz,71);
+
+    if(flag == 0)
+    {
+        printf ("O valor existe na arvore");
+    }
+    else
+    {
+        if(flag == 1)
+        {
+            printf ("O valor nao existe na arvore");
+        }
+        else
+        {
+            printf ("erro na busca !!");
+        }
+    }
+
+    // QUANTIDADE DE NOS INTERNOS
+
+    quantidade = -1;
+    nos_internos(arvore1->raiz,quantidade);
+
+    printf ("\n\n%d ",quantidade);
+
+    LiberaArvore(arvore1->raiz);
+
+    free(arvore1);
+    arvore1 = NULL;
+
+    return 0;
+}
